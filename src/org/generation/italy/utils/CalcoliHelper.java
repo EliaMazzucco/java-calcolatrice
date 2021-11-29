e9d6935334716d85e92bbdf26b7b84ccb2392f7d
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-29 17:14:38 +0100

    inserimento metodi valori assoluti massimi e minimi

--- src/org/generation/italy/utils/CalcoliHelper.java
+++ src/org/generation/italy/utils/CalcoliHelper.java
@@ -1,54 +1,89 @@
 package org.generation.italy.utils;
 
 public class CalcoliHelper {
-
+	
+	 private CalcoliHelper() {}
+	 
+	 ///////SOMMA///////
 	
 	public static int somma (int a, int b) {
 		return a + b;
 	}
 	
-	public static double sommaD(double a, double b) {
+	public static double somma(double a, double b) {
 		return a + b;
 	}
 	
-	
+	///////DIVISIONE///////
 	
 	public static int divisione(int a, int b) {
 		return a / b;
 	}
 	
-	public static double divisioneD(double a, double b) {
+	public static double divisione(double a, double b) {
 		return a / b;
 	}
 	
 	
-	
-	
+	///////MOLTIPLICAZIONE///////
 	
 	public static int moltiplicazione(int a, int b) {
 		return a * b;
 	}
-	public static double moltiplicazioneD(double a, double b) {
+	public static double moltiplicazione(double a, double b) {
 		return a * b;
 	}
 	
 	
+	///////VALORE ASSOLUTO///////
+	
+	public static int valoreAssoluto(int a) {
+		 if(a < 0) {
+			 a = a *-1;			 
+		 } return a;
+			
+    }
+		
+	
+		
+//	public static double valoreAssoluto(double a, double b) {}
 	
 	
-	public static int valoreAssoluto;
-	public static double valoreAssolutoD;
+	
+	///////VALORE MINIMO///////
+	public static int valoreMinimo(int a, int b) {
+		if (a < b) {
+			return a;			
+		} else return b;
+		
+	}
+	public static double valoreMinimo(double a, double b) {
+		if (a < b) {
+			return a;			
+		} else return b;
+		
+	}
 	
 	
+	///////VALRE MASSIMO///////
+	
+	public static int valoreMassimo(int a, int b) {
+		if (a > b) {
+			return a;			
+		} else return b;
+		
+	}
+	public static double valoreMassimo(double a, double b) {
+		if (a > b) {
+			return a;			
+		} else return b;
+		
+	}
 	
 	
-	public static int valoreMinimo;
-	public static double valoreMinimoD;
+	///////BONUS POTENZA///////
 	
 	
-	
-	
-	public static int valoreMassimo;
-	public static double valoreMassimoD;
 	
 	
 }
