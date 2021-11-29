6479b4904f94244bdff3067606766e738a78de11
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-29 16:02:24 +0100

    aggiunta di somme

--- src/org/generation/italy/utils/CalcoliHelper.java
+++ src/org/generation/italy/utils/CalcoliHelper.java
@@ -2,4 +2,53 @@ package org.generation.italy.utils;
 
 public class CalcoliHelper {
 
+	
+	public static int somma (int a, int b) {
+		return a + b;
+	}
+	
+	public static double sommaD(double a, double b) {
+		return a + b;
+	}
+	
+	
+	
+	public static int divisione(int a, int b) {
+		return a / b;
+	}
+	
+	public static double divisioneD(double a, double b) {
+		return a / b;
+	}
+	
+	
+	
+	
+	
+	public static int moltiplicazione(int a, int b) {
+		return a * b;
+	}
+	public static double moltiplicazioneD(double a, double b) {
+		return a * b;
+	}
+	
+	
+	
+	
+	public static int valoreAssoluto;
+	public static double valoreAssolutoD;
+	
+	
+	
+	
+	public static int valoreMinimo;
+	public static double valoreMinimoD;
+	
+	
+	
+	
+	public static int valoreMassimo;
+	public static double valoreMassimoD;
+	
+	
 }
